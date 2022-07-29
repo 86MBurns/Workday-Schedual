cbfe0187a252b5caee9eae52a8bfe9dce0810722
Matt Burns <matt.burns01@gmail.com>
2022-07-29 11:02:59 -0700

    i think my time blocks will work

--- Develop/script.js
+++ Develop/script.js
@@ -1,41 +1,39 @@
-var today = moment().format(yyyy-MM-dd);
+var today = moment().format(YYYY-MM-DD);
 $("#currentDay").html(today);
 
-//tracking time
- var timeBox = parseInt($(this).attr("id").split("hour")[1]);
- var currentTime = moment().hour();
 
- function time(){
-   
 
-    $(".time-block").each(functiuon (),{
+ 
+function currentMoment() {
+    var timeCurrent = moment().hour();
+
+    $(".time-block").each(function () {
+      var containerTime = parseInt($(this).attr("id").split("hour")[1]);
+
+      if (containerTime < timeCurrent) {
+        $(this).removeClass("future");
        
-
-        if (_timebox < currentTime) {
-            $(this).use(past);
-        } else if (timeBox < currentTime){
-            $(this).use(present);
-        }else if(timeBox <= currentTime){
-            $(this).use(future);
-        };
-
-        else if (timeBox <= currentTime) {
-            $(this).use(present);
-        } else if (timeBox  <= currentTime){
-            $(this).use(future);
-        }else if (timeBox <= currentTime);{
-            $(this).use(past);
-        };
-        else if (timeBox  <= currentTime) {
-            $(this).use(future);
-        } else if (timeBox  <= currentTime){
-            $(this).use(past);
-        }else (timeBox  <= currentTime);{
-            $(this).use(present);
-        }
+        $(this).removeClass("present");
+       
+        $(this).addClass("past");
+      } else if (blockTime === timeCurrent) {
+        $(this).removeClass("past");
+       
+        $(this).removeClass("future");
+       
+        $(this).addClass("present");
+      } else {
+        $(this).removeClass("present");
+       
+        $(this).removeClass("past");
+       
+        $(this).addClass("future");
+      }
     });
-}
+  }
 
 $("#hours").val(localStorage.getItem)("time-box");
 
-timeTracker();
+currentMoment();
+// save button
+
